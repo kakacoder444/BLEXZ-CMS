f8d9200d6020ea04eb6144e4709daad77862963e
Liakat hossain <liakat@blexz.com>
2017-11-11 20:40:48 +0600

    welcome page setup
    add welcome page and rearrange all structures

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Welcome</title>
+
+    <link rel="stylesheet" href="assets/bootstrap_3.3.7/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="container" style="background:#0163c3;text-align:center;color:white;">
+      <h1>Welcome to Blexz CMS!</h1>
+      <h2><a href="app/login.php" style="color:white;">Login</a> to play with me</h2>
+    </div>
+
+    <script src="assets/bootstrap_3.3.7/bootstrap.min.js"></script>
+    <script src="assets/jquery_3.2.1/jquery.min.js"></script>
+  </body>
+</html>
